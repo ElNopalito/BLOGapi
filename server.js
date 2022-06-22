8bb0da7529f36592134880401730040a2bff6b76
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-21 22:10:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const bcrypt = require('bcrypt')
+//!---------------------------------------------MongoDB--------------------------------------
 require('dotenv').config();
-
+//!---------------------------------------------Hero Roku------------------------------------
 const PORT = process.env.PORT || 3000;
 
+//!-------------------------------------------Modules Config---------------------------------
 const mongoConfig = require('./Config/mongoConfig');
 
-//!---------------------------------------------Importing Routers------------------------
+//!-----------------------------------------Importing Routers--------------------------------
 const authRouter = require('./Route/authRouter')
 const userRouter = require('./Route/userRouter');
 const blogRouter = require('./Route/blogRouter');
 
-//!-----------------------------------------------Modules Config------------------------------
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 
-//!---------------------------------------------Router Config----------------------------------
+//!--------------------------------------------Router Config-----------------------------------
 app.use('/Blog', blogRouter);
 app.use('/User', userRouter);
-app.use('/auth', authRouter)
-//!----------------------------------------MAIN SERVER------------------------------------- 
+app.use('/Auth', authRouter)
+//!--------------------------------------------MAIN SERVER-------------------------------------- 
 app.get('/', (req,res) => {
     res.status(200).json({message: 'Welcome to API'})
 })
-//!----------------------------------------APP LISTEN---------------------------------------
+//!---------------------------------------------APP LISTEN---------------------------------------
 app.listen(PORT, () => { 
     console.log(`Server is running on ${PORT}`);
     mongoConfig();
